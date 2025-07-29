531250ff9cb80368c29b863039e3ca94f0d1ba08
KingMaui <114877940+KingMaui@users.noreply.github.com>
2025-07-28 22:05:31 -0700

    Update product-script.js

--- java/product-script.js
+++ java/product-script.js
@@ -44,7 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
             <div class="product-info">
                 <h2>${product.title}</h2>
                 <p class="product-meta">${product.brand} • ${product.regions} • ${product.roast} Roast</p>
-                <p class="product-meta">${product.grind} • ${product.us-size}</p> <!-- Typo fixed here -->
                 
                 <div class="product-price">
                     $${product["sale price"] > 0 ? product["sale price"].toFixed(2) : product.price.toFixed(2)}
