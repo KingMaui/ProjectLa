f419fc6e91f636c3071aec923f2bf998f754039f
KingMaui <114877940+KingMaui@users.noreply.github.com>
2025-07-28 19:40:17 -0700

    Update store-script.js

--- java/store-script.js
+++ java/store-script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let allProducts = [];
 
     // Fetch and load products from JSON
-    fetch('products.json')
+    fetch('data/products.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
